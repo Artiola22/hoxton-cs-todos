1047657aa785115764ea24c6fcb9e95521166b7f
ArtiolaDomiCaka <artioladomi@hotmail.com>
2022-03-01 00:30:04 +0100

    not finished

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from '@testing-library/react'
+import {  render } from '@testing-library/react'
 import { describe , it, expect} from 'vitest'
 import { addTodo , App, removeTodo, todos} from './App'
 
